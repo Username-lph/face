d7e5b22c432fb68062121a9710f16fc2218a1e03
Username-lph <120lph@sina.com>
2018-12-01 17:01:44 +0800

    *

---
+++ public/js/flex.js
@@ -0,0 +1,66 @@
+// 原生js
+window.onload=function(){
+    // nameobj=document.getElementById('name');
+    // name2obj=document.getElementById('name2');
+    // areaobj=document.getElementById('area');
+    // area2obj=document.getElementById('area2');
+    // // onblur 当失去焦点的时候事件发生
+    // // onkeyup当键盘弹起的时候事件发生
+    // nameobj.onkeyup=function(){
+    //     val=this.value;
+    //     name2obj.innerHTML=val;
+    // }
+
+    // areaobj.onkeyup=function(){
+    //     val=this.value;
+    //     area2obj.innerHTML=val;
+    // }
+
+
+    // 回到顶部
+    // 判断是否为浏览器内核
+    // function isChorme(){
+    //     // 判断区分浏览器内核兼容性问题
+    //     // nu=navigator.userAgent;
+    //     nu=navigator.userAgent;
+    //     if(nu.match(/Chorme/i)){
+    //         return true;
+    //     }else{
+    //         return false;
+    //     }
+    // }
+    // // 获取元素
+    // scrollobj=document.getElementById('scrollTop');
+    
+    // // 当滚动滚动条时触发的事件，计算滚动条的位置;
+    // window.onscroll=function(){
+    //     if(isChorme()){
+    //         // 谷歌
+    //         st=document.body.scrollTop;
+    //         document.title=st;
+    //         if(st>200){
+    //             scrollobj.style.display="block";
+    //         }else{
+    //             scrollobj.style.display='none';
+    //         }
+    //     }else{
+    //         // 非谷歌
+    //         st=document.documentElement.scrollTop;
+    //         document.title=st;
+    //         if(st>200){
+    //             scrollobj.style.display='block';
+    //         }else{
+    //             scrollobj.style.display='none';
+    //         }
+    //     }
+        
+    // }
+    // // 当点击回到顶部图片时，scrollTop为0;
+    // scrollobj.onclick=function(){
+    //     if(isChorme()){
+    //         document.body.scrollTop=0;
+    //     }else{
+    //         document.documentElement.scrollTop=0;
+    //     }
+    // }
+}
---
+++ public/js/index.js
@@ -0,0 +1,74 @@
+// $(function(){
+//     $('#name').keyup(function(){
+//         val=this.value;
+//         // $('#name2').append(val);
+//         $('#name2').html(val);
+//     })
+//     $('#area').keyup(function(){
+//         val=this.value;
+//         $('#area2').append(val);
+//     })
+// })
+
+$(function(){
+    // // 判断是否为谷歌浏览器
+    // function isChorme(){
+    //     // 区分浏览器的内核
+    //     nu=navigator.userAgent;
+    //     if(nu.match(/Chorme/i)){
+    //         return true;
+    //     }else{
+    //         return false;
+    //     }
+    // }
+  
+
+    // scrollobj = $('.scrollTop');
+    
+    // $(window).scroll(function(){
+    //     if(isChorme()){
+    //         st = $(window).scrollTop();
+    //         document.title=st;
+    //         if(st>200){
+    //             scrollobj.show();
+    //         }else{
+    //             scrollobj.hide();
+    //         }
+    //     }else{
+    //         st = $(window).scrollTop();
+    //         document.title=st;
+    //         if(st>200){
+    //             scrollobj.show();
+    //         }else{
+    //             scrollobj.hide();
+    //         }
+    //     }
+    // })
+
+    // scrollobj.click(function(){
+    //     if(isChorme()){
+    //         document.body.scrollTop=0;
+    //     }else{
+    //         document.documentElement.scrollTop=0;
+    //     }
+    // })
+
+
+    // jQuery已兼容各个浏览器
+    $(window).scroll(function(){
+        if($(window).scrollTop()>200){
+            $('.gotop').fadeIn(400);
+        }else{
+            $('.gotop').fadeOut(400);
+        }
+    })
+
+    $('.gotop').click(function(event){
+        // 将防止上面的链接打开URL
+        event.preventDefault();
+        // 滚动条返回时的动画 
+        $('html,body').animate({'scrollTop':0},500);
+        return false;
+    })
+
+})
